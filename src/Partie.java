bb59e3584b45ba10318c32d9140a34d5e5c85140
19defroide <marc.defroidefond@gmail.com>
2020-01-31 13:21:08 +0100

    commit 2

--- src/Partie.java
+++ src/Partie.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import puissance4java.Grille;
+import puissance4java.joueur;
+
 /**
  * 
  */
@@ -7,5 +11,26 @@
  *
  */
 public class Partie {
+  
+  public joueur j1;
+  public joueur j2;
+  public Grille jeu;
+  
+  
+  public Partie() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nom du joueur 1 :");
+    String nom1 = scanner.nextLine();
+    System.out.println("Nom du joueur 2 :");
+    String nom2 = scanner.nextLine();
+    j1 = new joueur(0, 1, nom1);
+    j2 = new joueur(0, 2, nom2);
+    jeu = new Grille();
+    jeu.newGrille();
+  }
+  
+  while(!testGrillePleine) { //On mettra grille non pleine et pas de vainqueur
+    
+  }
 
 }
--- src/puissance4java/joueur.java
+++ src/puissance4java/joueur.java
@@ -4,11 +4,13 @@ public class joueur {
 	// Attributs////
 	public int nbvictoires;
 	public int numjoueur;
+	public String nom;
 
 	// Constructeur////
-	public joueur(int v, int num) {
+	public joueur(int v, int num, String name) {
 		this.nbvictoires = v;
 		this.numjoueur = num;
+		nom = name;
 	}
 
 }
