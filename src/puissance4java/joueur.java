ce7c6d85489e0e26cfe130cc3327a392d2c2d2a6
hhervier <hervierhadrien@gmail.com>
2020-01-31 11:49:21 +0100

    3e commit: création des classes

---
+++ src/puissance4java/joueur.java
@@ -0,0 +1,14 @@
+package puissance4java;
+
+public class joueur {
+	// Attributs////
+	public int nbvictoires;
+	public int numjoueur;
+
+	// Constructeur////
+	public joueur(int v, int num) {
+		this.nbvictoires = v;
+		this.numjoueur = num;
+	}
+
+}
